1e99196c13d03868b034a032ab3139319dd087cc
TTo3uTuBHbiu <ilya_ivanov_9797@mail.ru>
2017-05-31 20:21:11 +0300

    Corrections

--- src/Noughts.java
+++ src/Noughts.java
@@ -9,22 +9,25 @@ public class Noughts {
 
 
     public enum Sign {
-        NOUGHT,
-        CROSS;
+        NOUGHT ,
+        CROSS ;
 
-        public static String StringBuilder(Sign x) {
-            if (x == NOUGHT) return ("O ");
-            else if (x == CROSS) return ("X ");
-            else return ("- ");
-
+            public Sign opposite() {
+                if (this == NOUGHT) return CROSS;
+                else return NOUGHT;
+            }
         }
 
-    }
+
+
+
+
+
 
 
     public final static class Cell {
         int x;
-
+        Sign l = Sign.CROSS;
         int y;
 
         public Cell(int x, int y) {
@@ -123,7 +126,7 @@ public class Noughts {
 
         static private final Cell[] DIRECTIONS = new Cell[]{
                 new Cell(0, 1), new Cell(1, 0),
-                new Cell(1, 1), new Cell(1, -1)
+                new Cell(1, 1), new Cell(1, -1),
         };
 
         public boolean correct(Cell cell) {
@@ -132,6 +135,8 @@ public class Noughts {
 
 
         public int maxInRowSign(Sign x) {
+            String coordinates = "";
+            String maxCoordinates = "";
             int max = 1;
             for (int t = 0; t < width; ++t) {
 
@@ -148,17 +153,27 @@ public class Noughts {
                         Cell current = cell;
                         int length = 1;
                         for (; ; length++) {
+                            if ((coordinates.contains("(" + (current.getX()) + "," + (current.getY()) + ")")))
+                                coordinates = coordinates;
+                            else coordinates += "(" + (current.getX()) + "," + (current.getY()) + ")";
                             current = current.plus(dir);
                             if ((!correct(current)) || (get(current) != startSign)) break;
 
+
                         }
 
-                        if (length > max) max = length;
+                        if (length > max) {
+                            max = length;
+                            maxCoordinates = coordinates;
+                            coordinates = "";
+                        }
+                        coordinates = "";
 
 
                     }
                 }
             }
+            System.out.println(maxCoordinates);
             return max;
 
 
@@ -191,7 +206,8 @@ public class Noughts {
                         sb.append("- ");
                         continue;
                     }
-                    sb = sb.append(Sign.StringBuilder(sign));
+                    if (sign == Sign.NOUGHT)sb.append("O ");
+                    else if (sign == Sign.NOUGHT.opposite())sb.append("X ");
                 }
                 sb.append("\n");
             }
@@ -202,21 +218,21 @@ public class Noughts {
         public static void main(String args[]) {
             Board demo = new Board(4, 4);
             demo.addNought(0, 0);
-            demo.addCross(0, 1);
+            demo.addNought(0, 1);
             demo.addCross(0, 2);
-            demo.addNought(0, 3);
-            demo.addNought(1, 0);
+            demo.addCross(0, 3);
+            demo.addCross(1, 0);
             demo.addCross(1, 1);
             demo.addCross(1, 2);
-            demo.addCross(1, 3);
-            demo.addCross(2, 0);
+            demo.addNought(1, 3);
+            demo.addNought(2, 0);
             demo.addCross(2, 1);
-            demo.addNought(2, 2);
+            demo.addCross(2, 2);
             demo.addCross(2, 3);
-            demo.addCross(3, 0);
-            demo.addNought(3, 1);
-            demo.addCross(3, 2);
-            demo.addNought(3, 3);
+            demo.addNought(3, 0);
+            demo.addCross(3, 1);
+            demo.addNought(3, 2);
+            demo.addCross(3, 3);
 
 
             System.out.println(demo.toString());
